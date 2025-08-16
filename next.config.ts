7398cfddfe8d7e867b649d1ad45b69c18892c6f9
DaoTungMOB <thanhtungdao05@gmail.com>
2025-08-16 10:34:28 +0700

    change image config

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol:
-          (process.env.NEXT_PUBLIC_IMAGE_PROTOCOL as "http" | "https") ||
-          "http",
-        hostname: process.env.NEXT_PUBLIC_IMAGE_HOST || "localhost",
-        port: process.env.NEXT_PUBLIC_IMAGE_PORT || "",
-        pathname: "/uploads/**",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
