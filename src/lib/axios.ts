07c7839469bb600995b70584c4acbd353e2234ec
mmoc0910 <mmoc0910@gmail.com>
2025-10-31 16:53:00 +0700

    change env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosProgressEvent, AxiosRequestConfig} from "axios"
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
